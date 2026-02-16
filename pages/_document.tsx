4d10270bc9ab9506cb9b8dd3ca12fc3c6396141b
Silas Puterbaugh <silasputerbaugh1@gmail.com>
2026-02-15 18:00:56 -0600

    gergeOMGGG

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <NextHead>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
